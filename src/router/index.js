75cd4ae619198082c3e01358e82ad1386ab9e069
Thao Huynh <thao.huynh@navigosgroup.com>
2023-07-10 18:14:02 +0700

    add register

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/RegisterView'
 const routes = [
   {
     path: '/',
@@ -13,6 +14,10 @@ const routes = [
       {
         path: 'login',
         component: LoginView
+      },
+      {
+        path: 'register',
+        component: RegisterView
       }
     ]
   },
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,8 @@
 <template>
-  <div
-    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
-  >
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        class="mx-auto h-20 w-auto"
-        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-        alt="Your Company"
-      />
-      <h2
-        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
-      >
+      <img class="mx-auto h-20 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" />
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Sign in to your account
       </h2>
     </div>
@@ -19,44 +11,26 @@
       <form class="space-y-6" @submit.prevent="login()">
         <div>
           <div class="mt-2">
-            <input
-              placeholder="Email"
-              id="email"
-              name="email"
-              type="email"
-              v-model="loginForm.username"
+            <input placeholder="Email" id="email" name="email" type="email" v-model="loginForm.username"
               autocomplete="email"
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
         </div>
 
         <div>
           <div class="mt-2">
-            <input
-              placeholder="Password"
-              id="password"
-              name="password"
-              type="password"
-              v-model="loginForm.password"
+            <input placeholder="Password" id="password" name="password" type="password" v-model="loginForm.password"
               autocomplete="current-password"
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
             <div class="text-sm text-right">
-              <a
-                href="#"
-                class="font-semibold text-indigo-600 hover:text-indigo-500"
-                >Forgot password?</a
-              >
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
             </div>
           </div>
         </div>
 
         <div>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
+          <button type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
             Sign in
           </button>
         </div>
@@ -65,17 +39,12 @@
       <p class="mt-10 text-center text-sm text-gray-500">
         Not a member?
         {{ " " }}
-        <a
-          href="#"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >Create your business</a
-        >
+        <router-link to="/users/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create your business</router-link>
       </p>
     </div>
   </div>
 </template>
 <script>
-import { ref } from "vue";
 export default {
   name: "LoginView",
   components: {},
@@ -87,14 +56,10 @@ export default {
       },
     };
   },
-  setup() {
-    let loginForm = ref({ username: "", password: "" });
-    function login() {
-      debugger;
-      alert(loginForm.value.username);
+  methods: {
+    login() {
+      alert(this.loginForm.username);
     }
-
-    return { login };
-  },
+  }
 };
 </script>
