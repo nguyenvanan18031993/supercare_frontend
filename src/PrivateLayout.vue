33e6f59e45ea2e3feffca47d400b18b50d3c0882
Thao Huynh <thao.huynh@navigosgroup.com>
2023-07-13 17:51:48 +0700

    update

--- src/PrivateLayout.vue
+++ src/PrivateLayout.vue
@@ -4,7 +4,7 @@
     <div class="flex flex-wrap w-full h-screen">
       <div
         :class="[expand ? 'left__column__expand' : 'left__column']"
-        class="bg-white rounded shadow-lg transition-all duration-500 ease-in-out"
+        class="bg-white rounded shadow-lg transition-all duration-500 ease-in-out h-screen fixed"
       >
         <div class="p-3 border-b border-solid border-gray-200 ">
           <div class="flex items-center justify-center" style="height: 40px !important">
@@ -46,33 +46,10 @@
               />
             </div>
           </div>
-          <!--  <div>
-            <h4
-              class="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide"
-            >
-              James Bhatta
-            </h4>
-            <span class="text-sm tracking-wide flex items-center space-x-1">
-              <svg
-                class="h-4 text-green-500"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                /></svg
-              ><span class="text-gray-600">Verified</span>
-            </span> 
-          </div>-->
+          
         </div>
         <ul class="space-y-2 text-sm p-3">
           <li v-for="(nav, i) in navs" :key="`nav-item-${i}`">
-            <!-- <router-link to="/dashboard" class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline"> -->
             <router-link
               :to="nav.to"
               class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 focus:bg-gray-200 focus:shadow-outline"
@@ -104,15 +81,18 @@
         class="transition-all duration-500 ease-in-out right__column_expand"
       >
         <div class="px-4 text-gray-600">
-          <div class="flex justify-between border-b border-solid border-gray-200" style=" padding: 8px !important;" >
+          <div :class="[expand ? 'header__top__expand' : 'header__top']"
+           class="flex justify-between fixed bg-white" style=" padding: 8px !important;" >
             <h1 class="text-2xl flex items-center justify-center" >Users</h1>
             <div>
               <span>
-                <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <img class="h-10 w-10 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
               </span>
             </div>
           </div>
-          <router-view />
+          <div class="p-4" style="padding-top: 65px">
+            <router-view />
+          </div>
         </div>
       </div>
     </div>
@@ -145,13 +125,22 @@ export default {
   width: 3% !important;
 }
 .right__column {
-  width: 96% !important;
+  width: 99% !important;
+  padding-left: 3% !important;
 }
 .left__column__expand {
   width: 15% !important;
 }
 
 .right__column__expand {
-  width: 84% !important;
+  width: 99% !important;
+  padding-left: 15% !important;
+}
+
+.header__top{
+    width: 96% !important;
+}
+.header__top__expand{
+    width: 84% !important;
 }
 </style>
